d5a3ea6e8539627ac4f329399c3650c945c54093
emil <60194559+protofailure@users.noreply.github.com>
2024-11-22 17:24:10 -0500

    v1.2.0

--- mods/emilcosmetics/main.gd
+++ mods/emilcosmetics/main.gd
@@ -7,6 +7,10 @@ func _ready():
 	Lure.add_content("emilcosmetics", "halo_blue", "mod://Resources/Cosmetics/halo_blue.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
 	Lure.add_content("emilcosmetics", "halo_red", "mod://Resources/Cosmetics/halo_red.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
 	Lure.add_content("emilcosmetics", "halo_purple", "mod://Resources/Cosmetics/halo_purple.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
+	Lure.add_content("emilcosmetics", "broken_halo", "mod://Resources/Cosmetics/broken_halo.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
+	Lure.add_content("emilcosmetics", "broken_halo_blue", "mod://Resources/Cosmetics/broken_halo_blue.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
+	Lure.add_content("emilcosmetics", "broken_halo_red", "mod://Resources/Cosmetics/broken_halo_red.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
+	Lure.add_content("emilcosmetics", "broken_halo_purple", "mod://Resources/Cosmetics/broken_halo_purple.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
 	
 	# emotes
 	Lure.add_emote("emilcosmetics","griddy","mod://Assets/Animations/griddy.tres","mod://Assets/Icons/griddy.png","Get Griddy")
@@ -17,4 +21,5 @@ func _ready():
 	Lure.add_emote("emilcosmetics","nevergonna","mod://Assets/Animations/nevergonna.tres","mod://Assets/Icons/nevergonna.png","Never Gonna")
 	Lure.add_emote("emilcosmetics","bringitaround","mod://Assets/Animations/bringitaround.tres","mod://Assets/Icons/bringitaround.png","Bring It Around")
 	Lure.add_emote("emilcosmetics","boogiedown","mod://Assets/Animations/boogiedown.tres","mod://Assets/Icons/boogiedown.png","Boogie Down")
-	
+	Lure.add_emote("emilcosmetics","kuuneru_spin_l","mod://Assets/Animations/kuuneru_spin_l.tres","mod://Assets/Icons/kuuneru_spin_l.png","Kuuneru Engazer Spin (Left)")
+	Lure.add_emote("emilcosmetics","kuuneru_spin_r","mod://Assets/Animations/kuuneru_spin_r.tres","mod://Assets/Icons/kuuneru_spin_r.png","Kuuneru Engazer Spin (Right)")
